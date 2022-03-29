a83bedef37702afb225bedf97b1ac68a472d1246
Dane SAN <dane.g.san@gmail.com>
2022-03-29 16:18:04 +0200

    Section 2, Part 53

--- lib/result.dart
+++ lib/result.dart
@@ -8,10 +8,26 @@ class Result extends StatelessWidget {
     required this.resultScore,
   }) : super(key: key);
 
+  String get resultPhrase {
+    var resultText = "Vous avez terminé";
+    if (resultScore <= 8) {
+      resultText = "Vous êtes super et innocent";
+    } else if (resultScore <= 12) {
+      resultText = "Vous êtes assez sympa";
+    } else {
+      resultText = "Vous êtes le sheitan";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Vous avez terminé"),
+    return Center(
+      child: Text(
+        resultPhrase,
+        style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
